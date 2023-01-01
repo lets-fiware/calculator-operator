2ac2b07454bf8fbb1dd51d621a19cce12e25eed7
Kazuhito Suda <kazuhito@fisuda.jp>
2023-01-01 09:57:32 +0900

    UPDATE copyright date

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
  * Calculator Operator
  * https://github.com/lets-fiware/calculator-operator
  *
- * Copyright (c) 2019 Kazuhito Suda
+ * Copyright (c) 2019-2023 Kazuhito Suda
  * Licensed under the MIT license.
  */
 
